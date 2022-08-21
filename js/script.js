00592d7e168eaac30703cd84a95f101793f4c534
Programer Jr <yayoub2020@gmail.com>
2022-08-21 21:34:37 +0100

    Test

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+const navbarNav = document.querySelector('.navbar-nav');
+const navbarToggleBtn = document.querySelector('.nav-toggle-btn');
+
+navbarToggleBtn.addEventListener('click', function() {
+    navbarNav.classList.toggle('active');
+    this.classList.toggle('active');
+})
